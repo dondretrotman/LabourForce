0ca6ab4ef9d155c0d74dc4bf9b32211c232b9488
Dondre <dondre_t@hotmail.com>
2019-02-25 14:58:01 -0400

    Redid some changes
    Redid all changes to coder app and some changes to menu and labourfource

---
+++ Deploy/Deploy Coders.bat
@@ -0,0 +1,29 @@
+"Coding.pff"
+@echo off
+echo Please navigate to "\\172.28.10.215\d$" in explorer and log in with Administrator server credentials
+pause >nul
+@echo on
+rem Michelle
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\Auxil.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\CoderMenu.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\DataEntry.pen"
+echo Copied to Mbrathwaite
+
+rem Lisa
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\Auxil.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\CoderMenu.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\DataEntry.pen"
+echo Copied to Lsmith
+
+rem Roger
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\Auxil.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\CoderMenu.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\DataEntry.pen"
+echo Copied to Rblades
+
+rem Tameshia
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\Auxil.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\CoderMenu.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\DataEntry.pen"
+echo Copied to Twhite
+pause
