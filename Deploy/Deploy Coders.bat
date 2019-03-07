aad508b3f477cc86c6d98c6262da1478ce0b4fe7
Dondre <dondre_t@hotmail.com>
2019-03-06 21:10:02 -0400

    Implemented allocation module
    Supervisors can open any ED but enumerators have to be allocated

--- Deploy/Deploy Coders.bat
+++ Deploy/Deploy Coders.bat
@@ -7,23 +7,31 @@ rem Michelle
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\Auxil.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\CoderMenu.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Coding\DataEntry.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\OccupationcodesBARSOC.csdb" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Data\OccupationCodesBARSOC.csdb"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\IndustryCodes.csdb" "\\172.28.10.215\d$\Users\Desktop\mbrathwaite\Desktop\Coding\Data\IndustryCodes.csdb"
 echo Copied to Mbrathwaite
 
 rem Lisa
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\Auxil.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\CoderMenu.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Coding\DataEntry.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\OccupationcodesBARSOC.csdb" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Data\OccupationCodesBARSOC.csdb"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\IndustryCodes.csdb" "\\172.28.10.215\d$\Users\Desktop\lsmith\Desktop\Coding\Data\IndustryCodes.csdb"
 echo Copied to Lsmith
 
 rem Roger
-copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\Auxil.pen"
-copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\CoderMenu.pen"
-copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\C0ding\Coding\DataEntry.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\Coding\Coding\Auxil.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\Coding\Coding\CoderMenu.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\Coding\Coding\DataEntry.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\OccupationcodesBARSOC.csdb" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\Coding\Data\OccupationCodesBARSOC.csdb"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\IndustryCodes.csdb" "\\172.28.10.215\d$\Users\Desktop\rblades\Desktop\Coding\Data\IndustryCodes.csdb"
 echo Copied to Rblades
 
 rem Tameshia
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\Auxil.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\Auxil.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\CoderMenu.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\CoderMenu.pen"
 copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Coding\DataEntry.pen" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Coding\DataEntry.pen"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\OccupationcodesBARSOC.csdb" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Data\OccupationCodesBARSOC.csdb"
+copy "\\statserver.gov.bb\censussurveys\LBFORCE CAPI DATA\Coding\Data\IndustryCodes.csdb" "\\172.28.10.215\d$\Users\Desktop\twhite\Desktop\Coding\Data\IndustryCodes.csdb"
 echo Copied to Twhite
 pause
