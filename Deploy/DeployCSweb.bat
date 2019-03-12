46dea6051f19b6ab7b3373d34930b436525a152c
Dondre <dondre_t@hotmail.com>
2019-03-12 16:35:54 -0400

    Fixed display of selection lists

--- Deploy/DeployCSweb.bat
+++ Deploy/DeployCSweb.bat
@@ -1,6 +1,10 @@
 @echo off
+setlocal enabledelayedexpansion
 echo Please navigate to "\\172.28.10.220\c$" in explorer and log in with local server credentials
 pause >nul
 @echo on
 "CSweb Deploy.pff"
 "CSweb Update Deploy.pff"
+"Network Share Deploy.pff"
+@echo off
+
