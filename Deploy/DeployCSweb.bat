0d6b5749b6af7e95ef7bc697e02dc5a13e4581ed
Dondre <dondre_t@hotmail.com>
2019-03-26 10:36:20 -0400

    Fixed some fields being prefilled

--- Deploy/DeployCSweb.bat
+++ Deploy/DeployCSweb.bat
@@ -1,7 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
+rem for /F "tokens=* USEBACKQ" %%F in (`dir \\172.28.10.221\c$`) do (set exists=%%F)
+rem echo !exists!
+rem if  !exists!==Access^ is^ denied. (
 echo Please navigate to "\\172.28.10.220\c$" in explorer and log in with local server credentials
 pause >nul
+rem )
+
+:DEPLOY
 @echo on
 "CSweb Deploy.pff"
 "CSweb Update Deploy.pff"
