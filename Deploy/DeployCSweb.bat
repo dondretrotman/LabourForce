e4e9a9329ae1b5f1467a4c7a2f027e2ea334c9c2
Dondre <dondre_t@hotmail.com>
2020-07-07 13:16:01 -0400

    Made slight changes to question wording

--- Deploy/DeployCSweb.bat
+++ Deploy/DeployCSweb.bat
@@ -4,6 +4,7 @@ rem for /F "tokens=* USEBACKQ" %%F in (`dir \\172.28.10.221\c$`) do (set exists=
 rem echo !exists!
 rem if  !exists!==Access^ is^ denied. (
 echo Please navigate to "\\172.28.10.220\c$" in explorer and log in with local server credentials
+echo Then come back to this window and press "enter"
 %systemroot%\explorer.exe "172.28.10.220\c$"
 pause >nul
 rem )
